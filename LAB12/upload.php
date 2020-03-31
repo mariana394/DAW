98a1bf99d5a0b280c1bf0be8f0e59102628a2fd3
mariana394 <a01704671@itesm.mx>
2020-03-31 00:24:36 -0500

    lab13 terminado

---
+++ LAB12/upload.php
@@ -0,0 +1,61 @@
+ <?php
+ 
+    session_start();
+    
+    include("registrarcuenta.html");
+    
+    if (isset($_FILES["imagen"])) {
+        
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["imagen"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        
+            $check = getimagesize($_FILES["imagen"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["imagen"]["name"]). " has been uploaded.";
+                $_SESSION["imagen"] = $target_file;
+               
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+                
+            }
+        }
+        
+        
+    } else {
+        include("_formUpload.html");
+    }
+    
+    
+?>
